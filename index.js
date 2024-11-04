e1e1150365d5fbff2318cc8a9ed5618f1e18b2d9
Richard <mattsson.richard@gmail.com>
2024-11-04 13:59:06 +0100

    create function that loops through an array and calls the function that was sent in as an argument.

--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 // };
 // call(alert);
 // call(prompt);
-function g() {
-  let string = "Hello World!";
-  return string;
-}
-function f() {
-  return g;
-}
+// function g() {
+//   let string = "Hello World!";
+//   return string;
+// }
+// function f() {
+//   return g;
+// }
+// console.log(f()());
 
-console.log(f()());
+function forEach(a, f) {
+  for (let i = 0; i < a.length; i++) {
+    f(a[i]);
+  }
+}
+forEach([1, 2, 3], alert);
