61be807109ba20c3d6752318f0c3dfe1782360c9
Richard <mattsson.richard@gmail.com>
2024-11-04 15:02:53 +0100

    create function filter that creates a new array and returns the new array accordingly with the argument function.

--- index.js
+++ index.js
@@ -12,24 +12,36 @@
 // }
 // console.log(f()());
 
-function forEach(a, f) {
-  for (let i = 0; i < a.length; i++) {
-    f(a[i]);
-  }
-}
+// function forEach(a, f) {
+//   for (let i = 0; i < a.length; i++) {
+//     f(a[i]);
+//   }
+// }
 // forEach([1, 2, 3], alert);
 
-function find(a, f) {
+// function find(a, f) {
+//   for (let i = 0; i < a.length; i++) {
+//     if (f(a[i]) === true) {
+//       return a[i];
+//     }
+//   }
+// }
+// console.log(
+//   find([1, 2, 3], function (n) {
+//     return n > 2;
+//   })
+// );
+
+// console.log(find(["Hej", "Hejdå"], (s) => s.length === 5));
+
+function filter(a, f) {
+  let newArray = [];
   for (let i = 0; i < a.length; i++) {
     if (f(a[i]) === true) {
-      return a[i];
+      newArray.push(a[i]);
     }
   }
+  return newArray;
 }
-console.log(
-  find([1, 2, 3], function (n) {
-    return n > 2;
-  })
-);
 
-console.log(find(["Hej", "Hejdå"], (s) => s.length === 5));
+console.log(filter([5, 10, 15], (value) => value !== 5));
