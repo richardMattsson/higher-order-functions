81125c2bd1898cc8ab5282aa094d2113e15bc9cc
Richard <mattsson.richard@gmail.com>
2024-11-04 13:50:56 +0100

    create a function that returns a function that logs 'Hello World!'

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const call = function (g) {
-  g();
-};
-call(alert);
-call(prompt);
+// const call = function (g) {
+//   g();
+// };
+// call(alert);
+// call(prompt);
+function g() {
+  let string = "Hello World!";
+  return string;
+}
+function f() {
+  return g;
+}
+
+console.log(f()());
